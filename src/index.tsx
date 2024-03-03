1f6039f9953119b401b9c14975d5b5b5700bd1d8
Dhika P Ardana <dhikaardana87@gmail.com>
2024-03-04 01:02:01 +0700

    feat: fix permission request

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import { Platform, PermissionsAndroid } from 'react-native';
 export const getIMEI = async () => {
   try {
     if (Platform.OS === 'android') {
-      const granted = await PermissionsAndroid.request(
-        'android.permission.READ_PHONE_STATE'
-      );
+      const granted = await PermissionsAndroid.requestMultiple([
+        'android.permission.READ_PHONE_STATE',
+      ]);
 
       if (!granted) {
         throw 'Permission Un-granted';
